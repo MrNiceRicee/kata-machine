8963c3a33bf2dfcbb8f3effe442a0a0586289543
Joshua Santos <JoshCSan@gmail.com>
2023-09-26 10:43:57 -0700

    - update: do all the problems again

---
+++ src/day3/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let left = 0;
+    let right = haystack.length;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (haystack[mid] === needle) return true;
+
+        if (haystack[mid] > needle) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return false;
+}
